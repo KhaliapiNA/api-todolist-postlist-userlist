c7fe989c077be8709ea225a81f81315797d863c4
anastasia <nastya16.com@gmail.com>
2023-07-11 14:37:13 +0300

    new store todo and data transfer to TodoList.js with errors

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -1,7 +1,17 @@
-export function TodoList() {
+import {useDispatch, useSelector} from "react-redux";
+import {fetchTodos} from "../../redux/slice/todo";
+import "./TodoList.css"
+export function TodoList(props) {
+    const dispatch = useDispatch();
+    const state = useSelector((state) => state)
+    console.log("State", state)
     return(
         <div>
             <h1>TodoList</h1>
+            <div>
+
+                {/*{state.todo.map()}*/}
+            </div>
         </div>
     );
 }
---
+++ react-js3/src/redux/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from "@reduxjs/toolkit";
+import todoReduser from './slice/todo';
+
+export const store = configureStore(
+    {
+        reducer: {
+            todo: todoReduser,
+        }
+    }
+);
