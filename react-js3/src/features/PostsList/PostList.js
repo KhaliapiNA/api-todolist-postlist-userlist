fa63eaea9260cb13276bc6b63874460b2077f50d
anastasia <nastya16.com@gmail.com>
2023-07-12 10:16:47 +0300

    post styles added

--- react-js3/src/features/PostsList/PostList.js
+++ react-js3/src/features/PostsList/PostList.js
@@ -4,14 +4,27 @@ import {useSelector} from "react-redux";
 export function PostList() {
     const state = useSelector((state) => state)
     return (
-        <div className={"post-list"}>
+        <div className={"post-list-page"}>
             <h1>Post</h1>
+            <div className={"post-list"}>
                 {!state.post.data ? null : state.post.data.map((e) =>
                     <div className={"posts"}>
-                        <label>UserId: {e.userId}</label>
-                        <p className={"post-title"}>Title: {e.title}</p>
-                        <p class={"post-body"}>Text: {e.body}</p>
+                        <div className={"flex"}>
+                            <div className={"user-id-box"}>
+                                <span>UserId: </span>
+                                <p className={"user-id"}> {e.userId}</p>
+                            </div>
+                            <div className={"post-title-box"}>
+                                <span>Title: </span>
+                                <p className={"post-title"}>{e.title}</p>
+                            </div>
+                        </div>
+                        <div className={"text-body-box"}>
+                            <span>Text: </span>
+                            <p class={"post-body"}> {e.body}</p>
+                        </div>
                     </div>)}
+            </div>
         </div>
     );
 }
