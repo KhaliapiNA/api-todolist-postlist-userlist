a03265cf792cd579a772c94fbc3a7de259244ba1
anastasia <nastya16.com@gmail.com>
2023-07-12 10:32:57 +0300

    userlist styles added

--- react-js3/src/features/UserList/UserList.js
+++ react-js3/src/features/UserList/UserList.js
@@ -3,12 +3,12 @@ import "./UserList.css"
 export function UserList() {
     const state = useSelector((state) => state);
     return(
-        <div>
+        <div className={"user-list"}>
             <h1>UserList</h1>
             {!state.users.data ? null : state.users.data.map((e) =>
-                <div className={"posts"}>
-                    <p>{e.name}</p>
-                    <p>{e.username}</p>
+                <div className={"user-list-box"}>
+                    <p className={"user-list-name"}>{e.name}</p>
+                    <p className={"user-list-username"}>{e.username}</p>
                 </div>)}
         </div>
     );
