7e23c2efa0ed141035372aae7315b3fdfa2ae249
anastasia <nastya16.com@gmail.com>
2023-07-31 15:33:19 +0300

    done with axios, useState and useEffect

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -1,7 +1,22 @@
+import "./TodoList.css"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 export function TodoList() {
-    return(
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+            .then(res => console.log(setData(res.data)))
+            .catch(err => console.log(err));
+    }, [])
+    return (
         <div>
             <h1>TodoList</h1>
+            <div className={"todo-list"}>
+                {data.map((e, i) =>
+                    <li>
+                        <input key={i} type={"radio"} checked={e.completed} className={"radio"}/>{e.title}
+                    </li>)}
+            </div>
         </div>
     );
 }
--- react-js3/src/features/UserList/UserList.js
+++ react-js3/src/features/UserList/UserList.js
@@ -1,7 +1,21 @@
+import "./UserList.css"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 export function UserList() {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => setData(res.data))
+            .catch(err => console.log(err));
+    }, [])
     return(
-        <div>
+        <div className={"user-list"}>
             <h1>UserList</h1>
+            {data.map((e, i) =>
+                <div className={"user-list-box"}>
+                    <p key={i} className={"user-list-name"}>{e.name}</p>
+                    <p key={i} className={"user-list-username"} >{e.username}</p>
+                </div>)}
         </div>
     );
 }
