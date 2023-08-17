55c829c1b727e4f20df8d49048ad15da27893e28
anastasia <nastya16.com@gmail.com>
2023-08-17 12:44:44 +0300

    You need to make each of the  items clickable to be able to change the state of  the complete field. The layout can be any of your choice. first version

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -1,21 +1,38 @@
 import {useDispatch, useSelector} from "react-redux";
 import "./TodoList.css"
-import React, {useEffect} from 'react';
-import {fetchTodos} from "../../moduls/slice/todo.js";
+import React, {useState, useEffect} from 'react';
+import {fetchTodos, updateData} from "../../moduls/slice/todo.js";
 
 export function TodoList(props) {
+    const [data, setData] = useState([]);
     const dispatch = useDispatch();
     const state = useSelector((state) => state)
     useEffect(() => {
-        dispatch(fetchTodos());
+        dispatch(fetchTodos())
+            .then(jsonData => {
+            setData(jsonData);
+        });
     }, [dispatch]);
+    const handleUpdate = async (userId, newCompleted) => {
+        try {
+            await updateData(userId, newCompleted);
+
+            // Обновляем локальное состояние
+            setData(prevData =>
+                prevData.map(item => (item.userId === userId ? { ...item, completed: newCompleted } : item))
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+
     return (
         <div>
             <h1>TodoList</h1>
             <div className={"todo-list"}>
-                {!state.todo.data ? null :state.todo.data.map((e) =>
+                {!state.todo.data ? null : state.todo.data.map((e) =>
                     <li>
-                        <input key={e.userId} type={"radio"} checked={e.completed} className={"radio"}/>{e.title}
+                        <input key={e.userId} type={"radio"} checked={handleUpdate(e.userId, '')} className={"radio"}/>{e.title}
                     </li>)}
             </div>
         </div>
--- react-js3/src/moduls/slice/todo.js
+++ react-js3/src/moduls/slice/todo.js
@@ -4,6 +4,15 @@ export const fetchTodos = createAsyncThunk('fetchTodos', async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/todos');
     return response.json();
 });
+export const updateData = async (id, newCompleted) => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({completed: newCompleted}),
+    });
+}
 const todoSlice = createSlice({
     name: 'todo',
     initialState: {
