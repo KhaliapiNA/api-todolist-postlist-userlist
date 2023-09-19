9c5de664e8abd81c787e82bfb04933643867c652
anastasia <nastya16.com@gmail.com>
2023-09-19 04:35:55 +0300

    TodoList.js almost done

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -1,7 +1,7 @@
 import {useDispatch, useSelector} from "react-redux";
 import "./TodoList.css"
 import React, {useState, useEffect} from 'react';
-import {fetchTodos, updateData} from "../../moduls/slice/todo.js";
+import {fetchTodos, toggleComplete} from "../../moduls/slice/todo.js";
 
 export function TodoList(props) {
     const [data, setData] = useState([]);
@@ -13,19 +13,11 @@ export function TodoList(props) {
             setData(jsonData);
         });
     }, [dispatch]);
-    const handleUpdate = async (userId, newCompleted) => {
-        try {
-            await updateData(userId, newCompleted);
-
-            // Обновляем локальное состояние
-            setData(prevData =>
-                prevData.map(item => (item.userId === userId ? { ...item, completed: newCompleted } : item))
-            );
-        } catch (error) {
-            console.log(error.message);
-        }
+    const  handleCompleteClick = ()=>{
+        dispatch(
+            toggleComplete({userId: state.todo.userId, completed: !state.todo.completed})
+        );
     };
-
     return (
         <div>
             <h1>TodoList</h1>
@@ -33,7 +25,7 @@ export function TodoList(props) {
                 {!state.todo.data ? null : state.todo.data.map((e) =>
                     <li>
                         <input key={e.userId} type={"radio"}
-                               checked={e.completed}
+                               onChange={()=>handleCompleteClick}
                                className={"radio"}/>
                         {e.title}
                     </li>)}
