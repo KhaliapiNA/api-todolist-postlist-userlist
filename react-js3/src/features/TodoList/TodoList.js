d7eb4c412b9307d09f6216f497f7319897e5683a
anastasia <nastya16.com@gmail.com>
2023-09-22 04:55:50 +0300

    todolist share on tosoitems

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -2,33 +2,15 @@ import {useDispatch, useSelector} from "react-redux";
 import "./TodoList.css"
 import React, {useState, useEffect} from 'react';
 import {fetchTodos, toggleComplete} from "../../moduls/slice/todo.js";
+import {TodoItem} from "./TodoItem/TodoItem";
 
 export function TodoList(props) {
-    const [data, setData] = useState([]);
-    const dispatch = useDispatch();
-    const state = useSelector((state) => state)
-    useEffect(() => {
-        dispatch(fetchTodos())
-            .then(jsonData => {
-            setData(jsonData);
-        });
-    }, [dispatch]);
-    const  handleCompleteClick = ()=>{
-        dispatch(
-            toggleComplete({userId: state.todo.userId, completed: !state.todo.completed})
-        );
-    };
+
     return (
         <div>
             <h1>TodoList</h1>
             <div className={"todo-list"}>
-                {!state.todo.data ? null : state.todo.data.map((e) =>
-                    <li>
-                        <input key={e.userId} type={"radio"}
-                               onChange={()=>handleCompleteClick}
-                               className={"radio"}/>
-                        {e.title}
-                    </li>)}
+                <TodoItem/>
             </div>
         </div>
     );
