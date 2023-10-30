752b26d342642b8455dfb4d3b6fc30de3067dbcc
anastasia <nastya16.com@gmail.com>
2023-10-30 21:31:07 +0200

    version with toggleComplete(not working)

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -1,38 +1,29 @@
 import {useDispatch, useSelector} from "react-redux";
 import "./TodoList.css"
 import React, {useState, useEffect} from 'react';
-import {fetchTodos, updateData} from "../../moduls/slice/todo.js";
+import todo, {fetchTodos, updateData} from "../../moduls/slice/todo.js";
 
 export function TodoList(props) {
     const [data, setData] = useState([]);
     const dispatch = useDispatch();
     const state = useSelector((state) => state)
     useEffect(() => {
-        dispatch(fetchTodos())
-            .then(jsonData => {
-            setData(jsonData);
-        });
-    }, [dispatch]);
-    const handleUpdate = async (userId, newCompleted) => {
-        try {
-            await updateData(userId, newCompleted);
-
-            // Обновляем локальное состояние
-            setData(prevData =>
-                prevData.map(item => (item.userId === userId ? { ...item, completed: newCompleted } : item))
-            );
-        } catch (error) {
-            console.log(error.message);
-        }
-    };
-
+        dispatch(fetchTodos(),
+            [dispatch])
+    });
+    const toggleComplete = id => {
+        setData(data.map(e => e.id === id ? {e, completed: !e.completed} : e))
+    }
     return (
         <div>
             <h1>TodoList</h1>
             <div className={"todo-list"}>
                 {!state.todo.data ? null : state.todo.data.map((e) =>
                     <li>
-                        <input key={e.userId} type={"radio"} checked={handleUpdate(e.userId, '')} className={"radio"}/>{e.title}
+                        <input key={e.id} type={"radio"}
+                               checked={e.completed}
+                               onChange={() => toggleComplete(e.id)}
+                               className={"radio"}/>{e.title}
                     </li>)}
             </div>
         </div>
