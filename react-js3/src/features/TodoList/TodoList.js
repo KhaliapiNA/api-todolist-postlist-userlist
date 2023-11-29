688c0ec11329027f7663a80fc1a89901d4eac57b
anastasia <nastya16.com@gmail.com>
2023-11-29 14:25:52 +0200

    changes in map, dispatch

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -2,7 +2,6 @@ import {useDispatch, useSelector} from "react-redux";
 import "./TodoList.css"
 import {useState, useEffect} from 'react';
 import {fetchTodos} from "../../moduls/slice/todo.js";
-import { connect } from 'react-redux';
 
 export function TodoList(props) {
 
@@ -10,28 +9,42 @@ export function TodoList(props) {
     const dispatch = useDispatch();
     const state = useSelector((state) => state)
     useEffect(() => {
+        dispatch(fetchTodos())
+            .then(jsonData => {
+                setTasks(jsonData);
+            });
+    }, [dispatch]);
+ /*   const  handleComplete = ()=>{
+        dispatch(
+            toggleComplete({id: state.todo.id, completed: !state.todo.completed})
+        );
+    };*/
+
+
+
+    /*useEffect(() => {
         dispatch(setTasks(fetchTodos()),
             [dispatch])
-    });
+    });*/
 
-    function changeStatus(dataId, isDone) {
-        let task = tasks.payload.find(e => e.id === dataId);
-        if (task) {
-            task.completed = isDone;
-        }
-        setTasks([...tasks]);
+       function changeStatus(taskId) {
+           let task = tasks.payload.find(e => e.id === taskId);
+           if (task) {
+               task.completed = !task.completed;
+           }
+           setTasks([...tasks]);
 
-    }
+       }
 
 
 
     const handleCompleteClick = () => {
-       /* dispatch(setTasks({
-                id: state.todo.id,
-                completed: !state.todo.completed
-            })
-        );*/
-        console.log(tasks)
+        /* dispatch(setTasks({
+                 id: state.todo.id,
+                 completed: !state.todo.completed
+             })
+         );*/
+        console.log(tasks.payload)
     };
 
 
@@ -41,7 +54,7 @@ export function TodoList(props) {
             <div className={"todo-list"}>
                 {!tasks.payload ? null : tasks.payload.map((e) => {
                         const handlerOnChange = (event) => {
-                            changeStatus(e.id, event.currentTarget.checked)
+                            console.log(e.id, event.currentTarget.completed)
                         }
                         return (
                             <li key={e.id}>
