b6787fc47e9e3f63af673eb3495ec223b9b6e103
anastasia <nastya16.com@gmail.com>
2023-11-29 14:02:15 +0200

    changes in map, dispatch

--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -1,37 +1,48 @@
 import {useDispatch, useSelector} from "react-redux";
 import "./TodoList.css"
 import {useState, useEffect} from 'react';
-import todoSlice, {fetchTodos} from "../../moduls/slice/todo.js";
-import {store} from "../../moduls/store";
-import todo from "../../moduls/slice/todo.js";
+import {fetchTodos} from "../../moduls/slice/todo.js";
+import { connect } from 'react-redux';
 
 export function TodoList(props) {
+
     const [tasks, setTasks] = useState([]);
     const dispatch = useDispatch();
     const state = useSelector((state) => state)
     useEffect(() => {
-        dispatch(fetchTodos(),
+        dispatch(setTasks(fetchTodos()),
             [dispatch])
     });
 
     function changeStatus(dataId, isDone) {
-        let d = tasks.find(e => e.id === dataId);
-        if (d) {
-           d.isDone = isDone;
+        let task = tasks.payload.find(e => e.id === dataId);
+        if (task) {
+            task.completed = isDone;
         }
         setTasks([...tasks]);
-        console.log(tasks)
+
     }
+
+
+
+    const handleCompleteClick = () => {
+       /* dispatch(setTasks({
+                id: state.todo.id,
+                completed: !state.todo.completed
+            })
+        );*/
+        console.log(tasks)
+    };
 
 
     return (
         <div>
             <h1>TodoList</h1>
             <div className={"todo-list"}>
-                {!state.todo.data ? null : state.todo.data.map((e) => {
-                    const handlerOnChange = (event) => {
-                        changeStatus(e.id, event.currentTarget.checked)
-                    }
+                {!tasks.payload ? null : tasks.payload.map((e) => {
+                        const handlerOnChange = (event) => {
+                            changeStatus(e.id, event.currentTarget.checked)
+                        }
                         return (
                             <li key={e.id}>
                                 <input type={"checkbox"}
