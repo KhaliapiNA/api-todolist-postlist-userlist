c0442d68a936e8427e660a0e6c8c1b2737d72986
anastasia <nastya16.com@gmail.com>
2023-11-30 18:06:51 +0200

    changes in handlerOnChange and changeStatus

--- react-js3/src/features/PostsList/PostList.js
+++ react-js3/src/features/PostsList/PostList.js
@@ -18,16 +18,16 @@ export function PostList() {
                             <div className={"flex"}>
                                 <div className={"user-id-box"}>
                                     <span>UserId: </span>
-                                    <p key={e.userId} className={"user-id"}> {e.userId}</p>
+                                    <p key={e.id} className={"user-id"}> {e.userId}</p>
                                 </div>
                                 <div className={"post-title-box"}>
                                     <span>Title: </span>
-                                    <p key={e.userId} className={"post-title"}>{e.title}</p>
+                                    <p key={e.id} className={"post-title"}>{e.title}</p>
                                 </div>
                             </div>
                             <div className={"text-body-box"}>
                                 <span>Text: </span>
-                                <p key={e.userId} class={"post-body"}> {e.body}</p>
+                                <p key={e.id} class={"post-body"}> {e.body}</p>
                             </div>
                         </div>)}
                 </div>
--- react-js3/src/features/TodoList/TodoList.js
+++ react-js3/src/features/TodoList/TodoList.js
@@ -4,12 +4,10 @@ import {useState, useEffect} from 'react';
 import todo, {fetchTodos} from "../../moduls/slice/todo.js";
 
 export function TodoList(props) {
-    const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([fetchTodos()]);
     const tasksState = tasks.payload;
     const dispatch = useDispatch();
-    const state = useSelector((state) => state)
     console.log(tasksState)
-
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -19,41 +17,32 @@ export function TodoList(props) {
                 console.log("error: ", error)
             }
         };
+        fetchData()
     }, [dispatch]);
 
     function changeStatus(taskId, taskCompleted) {
         let task = tasksState.find(e => e.id === taskId);
-        // if (task) {
-        //task.completed === !taskCompleted;
-        //}
-        //([...tasksState]);
-
+        if (task) {
+            task.completed = !taskCompleted;
+        }
+        setTasks([...tasksState]);
 
     }
 
-    const handleCompleteClick = () => {
-        setTasks(
-            Object.defineProperties(...tasksState,
-                {
-                    id: tasksState.id,
-                    completed: !tasksState.completed
-                })
-        );
-    };
+
     return (
         <div>
             <h1>TodoList</h1>
             <div className={"todo-list"}>
                 {!tasksState ? null : tasksState.map((e) => {
-                        const handlerOnChange = (event) => {
-                            changeStatus(e.id, !event.currentTarget.checked)
-
-                        }
+                    const handlerOnChange = (event) => {
+                        changeStatus(e.id, event.currentTarget.checked)
+                    }
                         return (
                             <li key={e.id}>
                                 <input type={"checkbox"}
                                        checked={e.completed}
-                                       onChange={handleCompleteClick}
+                                       onChange={handlerOnChange}
                                        className={"radio"}/>
                                 {e.title}
                             </li>)
